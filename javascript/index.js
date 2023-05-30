99af7a8dda87840394f5ae17546bd4d05cb7a7ed
deiby vargas <vargaschavesdeiby@gmail.com>
2023-05-29 20:51:01 -0600

    order

--- javascript/index.js
+++ javascript/index.js
@@ -1,81 +1,79 @@
 function getComputerChoice() {
     var randomNumber = Math.floor(Math.random() * 3) + 1;
     if (randomNumber === 3) {
-      return 'rock';
+    return 'rock';
     } else if (randomNumber === 2) {
-      return 'paper';
+    return 'paper';
     } else {
-      return 'scissors';
+    return 'scissors';
     }
-  }
-  
-  function playRound(playerSelection, computerSelection) {
+}
+
+function playRound(playerSelection, computerSelection) {
     if (
-      (playerSelection === 'rock' && computerSelection === 'rock') ||
-      (playerSelection === 'paper' && computerSelection === 'paper') ||
-      (playerSelection === 'scissors' && computerSelection === 'scissors')
+    (playerSelection === 'rock' && computerSelection === 'rock') ||
+    (playerSelection === 'paper' && computerSelection === 'paper') ||
+    (playerSelection === 'scissors' && computerSelection === 'scissors')
     ) {
-      return 'tie';
+    return 'tie';
     } else if (
-      (playerSelection === 'rock' && computerSelection === 'scissors') ||
-      (playerSelection === 'paper' && computerSelection === 'rock') ||
-      (playerSelection === 'scissors' && computerSelection === 'paper')
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
     ) {
-      return 'win';
+    return 'win';
     } else if (
-      (playerSelection === 'rock' && computerSelection === 'paper') ||
-      (playerSelection === 'paper' && computerSelection === 'scissors') ||
-      (playerSelection === 'scissors' && computerSelection === 'rock')
+    (playerSelection === 'rock' && computerSelection === 'paper') ||
+    (playerSelection === 'paper' && computerSelection === 'scissors') ||
+    (playerSelection === 'scissors' && computerSelection === 'rock')
     ) {
-      return 'lose';
-    }
-  }
-  
-  function game() {
+    return 'lose';
+    }}
+
+function game() {
     var playerScore = 0;
     var computerScore = 0;
-  
+
     for (var i = 0; i < 5; i++) {
-      var userInput = prompt("Select: paper, scissors, or rock:");
-      var lowercaseInput = userInput.toLowerCase();
-      var playerSelection = checkPrompt(lowercaseInput);
-      var computerSelection = getComputerChoice();
-  
-      if (playerSelection === null) {
+    var userInput = prompt("Select: paper, scissors, or rock:");
+    var lowercaseInput = userInput.toLowerCase();
+    var playerSelection = checkPrompt(lowercaseInput);
+    var computerSelection = getComputerChoice();
+
+    if (playerSelection === null) {
         console.log("Invalid option. Round skipped.");
         continue;
-      }
-  
-      var result = playRound(playerSelection, computerSelection);
-  
-      if (result === 'win') {
+    }
+
+    var result = playRound(playerSelection, computerSelection);
+
+    if (result === 'win') {
         playerScore++;
         console.log("You win this round!");
-      } else if (result === 'lose') {
+    } else if (result === 'lose') {
         computerScore++;
         console.log("You lose this round.");
-      } else {
+    } else {
         console.log("It's a tie this round.");
-      }
     }
-  
+    }
+
     console.log("Game Over!");
     console.log("Player Score: " + playerScore);
     console.log("Computer Score: " + computerScore);
-  
+
     if (playerScore > computerScore) {
-      console.log("Congratulations! You won the game!");
+    console.log("Congratulations! You won the game!");
     } else if (playerScore < computerScore) {
-      console.log("Sorry, you lost the game.");
+    console.log("Sorry, you lost the game.");
     } else {
-      console.log("It's a tie game.");
+    console.log("It's a tie game.");
     }
-  }
-  
-  function checkPrompt(lowercaseInput) {
+}
+
+function checkPrompt(lowercaseInput) {
     var possibleValues = ["rock", "paper", "scissors"];
     return possibleValues.includes(lowercaseInput) ? lowercaseInput : null;
-  }
-  
-  game();
-  
+}
+
+game();
